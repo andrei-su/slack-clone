140bc147671b0f7d8550971ebf65d6462f4c04d0
andrei <git1@email.com>
2021-09-11 01:15:36 +0200

    added icons in Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,20 +3,24 @@ import React from "react";
 import { useParams } from "react-router";
 // Styles
 import "./Chat.css";
+// Icons
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
 function Chat() {
   const { roomId } = useParams();
   return (
     <div className="chat">
-      <h2>Your are in the {roomId} room</h2>
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
             <strong># general</strong>
+            <StarBorderOutlinedIcon />
           </h4>
         </div>
         <div className="chat__headerRight">
           <p>
+            <InfoOutlinedIcon /> Details
           </p>
         </div>
       </div>
