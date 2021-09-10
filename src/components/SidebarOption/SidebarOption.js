f8be1977fa8fa299c33eb167b8f922688d789fd6
andrei <git1@email.com>
2021-09-10 20:08:52 +0200

    added SidebarOption implemetation

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -2,8 +2,19 @@ import React from "react";
 // Styles
 import "./SidebarOption.css";
 
-function SidebarOption() {
-  return <div></div>;
+function SidebarOption({ Icon, title }) {
+  return (
+    <div className="sidebarOption">
+      {Icon && <Icon className="sidebarOption__icon" />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <h3 className='sidebarOption__channel'>
+          <span className="sidebarOption__hash">#</span> {title}
+        </h3>
+      )}
+    </div>
+  );
 }
 
 export default SidebarOption;
