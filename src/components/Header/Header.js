469f2c7eb6159944e1b642de17b5c7c968267b7e
andrei <git1@email.com>
2021-09-10 17:52:12 +0200

    added header scheleton

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+// Styles
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatars for logged in user */}
+        {/* Time icon */}
+      </div>
+      <div className="header__search">
+        {/* Search icon */}
+        {/* input */}
+      </div>
+      <div className="header__right">{/* help icon */}</div>
+    </div>
+  );
+}
+
+export default Header;
