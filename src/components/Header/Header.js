2caab176ad2220dc865c2c8e0df932a041f22a8e
andrei <git1@email.com>
2021-09-10 18:12:52 +0200

    added icons in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from "react";
 // Styles
 import "./Header.css";
+// Avatar
+import { Avatar } from "@material-ui/core";
+// Icons
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        {/* Avatars for logged in user */}
-        {/* Time icon */}
+        <Avatar
+          className="header__avatar"
+          alt='User Name'
+          src=''
+        />
+        <AccessTimeIcon />
       </div>
       <div className="header__search">
-        {/* Search icon */}
-        {/* input */}
+        <SearchIcon />
+        <input placeholder="Search" />
       </div>
-      <div className="header__right">{/* help icon */}</div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
     </div>
   );
 }
