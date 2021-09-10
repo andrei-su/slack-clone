eb715cc4dea009b3912039b7c91e057e5fa97652
andrei <git1@email.com>
2021-09-10 23:27:29 +0200

    added router structure
    adding react router wrapper basic structure and routes, routing system, skeleton

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import "./App.css";
 // Components
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+// React Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM naming convention
     // app instead of App for class name
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
-      {/* React-Router -> Chat screen */}
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+            <Route path="/room/:roomId">
+              <h1>Chat screen</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
