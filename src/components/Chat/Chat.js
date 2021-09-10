a3a71c033b4f85c769745338a10a23700b1883de
andrei <git1@email.com>
2021-09-11 01:04:40 +0200

    updated Chat component
    updated Chat.js component main structure and skeleton

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,6 +9,17 @@ function Chat() {
   return (
     <div className="chat">
       <h2>Your are in the {roomId} room</h2>
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">
+            <strong># general</strong>
+          </h4>
+        </div>
+        <div className="chat__headerRight">
+          <p>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
