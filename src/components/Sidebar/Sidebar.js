acf8f9281c42945bf7c64619b136b64a9371db13
andrei <git1@email.com>
2021-09-11 01:19:27 +0200

    remove implemented skeleton commnet

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -59,8 +59,6 @@ function Sidebar() {
       {channels.map((channel) => (
         <SidebarOption title={channel.name} id={channel.id} />
       ))}
-      {/* connect to DB and list all the channels */}
-      {/* <SidebarOption ... /> */}
     </div>
   );
 }
