7611e2643ef2e4be4bb39e56366adf7fd2de43f0
andrei <git1@email.com>
2021-09-11 17:46:00 +0200

    added channels routing
    added channels routing system using BrowserHistory from history dependency

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,15 +1,33 @@
 import React from "react";
+// History
+import { createBrowserHistory } from "history";
 // Styles
 import "./SidebarOption.css";
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
+  const history = createBrowserHistory();
+
+  const selectChannel = () => {
+    if (id) {
+      history.push(`/room/${id}`);
+    } else {
+      history.push(title);
+    }
+  };
+
+  const addChannel = () => {
+  };
+
   return (
-    <div className="sidebarOption">
+    <div
+      className="sidebarOption"
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? (
         <h3>{title}</h3>
       ) : (
-        <h3 className='sidebarOption__channel'>
+        <h3 className="sidebarOption__channel">
           <span className="sidebarOption__hash">#</span> {title}
         </h3>
       )}
