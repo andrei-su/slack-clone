e72a9c30a310f5a66a31b96abdd850e13e4c06d5
andrei <git1@email.com>
2021-12-12 19:45:08 +0100

    Changed Firebase deploy options
    * Changed Firebase deploy project
    * Updated CI/CD GitHub auto deploy
    * Change Project reference Config file

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,12 @@ import {
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBt0gIOxPeeyo763mn22EKvEvf-7V7Ee6k",
-  authDomain: "slack-clone-9.firebaseapp.com",
-  projectId: "slack-clone-9",
-  storageBucket: "slack-clone-9.appspot.com",
-  messagingSenderId: "885443963017",
-  appId: "1:885443963017:web:bd0e180dba7dca17c87e58",
+  apiKey: "AIzaSyAsN-NRC3iArMfE0gypNuDbMyLDu5CJsr4",
+  authDomain: "clone-projects-9.firebaseapp.com",
+  projectId: "clone-projects-9",
+  storageBucket: "clone-projects-9.appspot.com",
+  messagingSenderId: "174316682985",
+  appId: "1:174316682985:web:ab1cf64bfead39386dba78"
 };
 
 initializeApp(firebaseConfig);
